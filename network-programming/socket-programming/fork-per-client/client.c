402944228ee6c741f51a9dbea37b7b058cd246ea
Karuppiah Natarajan <karuppiah7890@gmail.com>
2026-05-28 17:42:39 +0530

    add server with fork per client

---
+++ network-programming/socket-programming/fork-per-client/client.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+
+int main()
+{
+    int sock = 0;
+    struct sockaddr_in server_addr;
+
+    char buffer[1024] = {0};
+
+    // 1. Create socket
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sock == -1)
+    {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Client Socket File Descriptor: %d\n", sock);
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+
+    // Convert IP address text -> binary
+    inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
+
+    // 2. Connect to server
+    if (connect(sock,
+                (struct sockaddr *)&server_addr,
+                sizeof(server_addr)) < 0)
+    {
+        perror("connect failed");
+        if (close(sock) < 0)
+        {
+            perror("error closing client socket file descriptor");
+        }
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Connected to server\n");
+
+    // 3. Send message
+    char *message = "Hello from client";
+    send(sock, message, strlen(message), 0);
+
+    // 4. Receive response
+    recv(sock, buffer, sizeof(buffer), 0);
+
+    printf("Server replied: %s\n", buffer);
+
+    if (close(sock) < 0)
+    {
+        perror("error closing client socket file descriptor");
+    }
+
+    return 0;
+}
---
+++ network-programming/socket-programming/fork-per-client/server.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define PORT 8080
+
+void handle_client(int client_socket)
+{
+    char buffer[1024] = {0};
+
+    printf("Client connected!\n");
+
+    printf("Client Socket File Descriptor: %d\n", client_socket);
+
+    // 5. Read data from client
+    read(client_socket, buffer, sizeof(buffer));
+    printf("Client says: %s\n", buffer);
+
+    // 6. Send response
+    char *response = "Hello from server";
+    send(client_socket, response, strlen(response), 0);
+
+    printf("Response sent\n");
+}
+
+int main()
+{
+    // server file descriptor
+    int server_fd;
+    int client_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+
+    // 1. Create socket
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (server_fd == -1)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server Socket File Descriptor: %d\n", server_fd);
+
+    int opt = 1;
+
+    // TCP has a concept called TIME_WAIT
+    // which causes "bind failed: Address already in use"
+    // even though there's no other process bound to the
+    // address. In order to overcome the error, we say
+    // "allow local address reuse"
+    setsockopt(server_fd,
+               SOL_SOCKET,
+               SO_REUSEADDR,
+               &opt,
+               sizeof(opt));
+
+    // Configure server address
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // 2. Bind socket to IP + PORT
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // 3. Listen for incoming connections
+    if (listen(server_fd, 5) < 0)
+    {
+        perror("listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server listening on port %d...\n", PORT);
+
+    while (1)
+    {
+        // 4. Accept a client connection
+        client_socket = accept(server_fd,
+                               (struct sockaddr *)&address,
+                               (socklen_t *)&addrlen);
+
+        if (client_socket < 0)
+        {
+            perror("accept failed");
+            exit(EXIT_FAILURE);
+        }
+
+        pid_t pid = fork();
+
+        if (pid == 0)
+        {
+            // Child process
+
+            // As child process does not need the server socket file descriptor
+            // as that's only for accepting clients and server has a copy of
+            // the file descriptor table which has the same file descriptor
+            // value pointing to the actual kernel socket object reference
+            // in the kernel and can accept clients with it. The kernel socket
+            // object reference count is tracked - so, even if one kernel
+            // socket object reference is there, the socket is not closed.
+            // close(socket) simply removes the reference in the process
+            // file descriptor table - so, if there are other references,
+            // for example in the parent or child process since the
+            // file descriptor tables are copied to child from parent
+            // during fork(), then the kernel destroys the socket (garbage
+            // collection) only if all references are removed
+            if (close(server_fd) < 0)
+            {
+                perror("error closing server socket file descriptor in child process");
+            }
+
+            handle_client(client_socket);
+
+            if (close(client_socket) < 0)
+            {
+                perror("error closing client socket file descriptor in child process");
+            }
+
+            exit(0);
+        }
+
+        // As the server process does not require the client socket
+        // file descriptor as the client has access to it and child
+        // process has reference to the kernel socket object so the
+        // socket is not destroyed until all references are removed
+        if (close(client_socket) < 0)
+        {
+            perror("error closing client socket file descriptor in parent process");
+        }
+    }
+
+    if (close(server_fd) < 0)
+    {
+        perror("error closing server socket file descriptor in parent process");
+    }
+
+    return 0;
+}
